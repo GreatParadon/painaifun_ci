71341d2550c75fb2f708df5a58e050301bb2d7e4
Paradon Wisdomlanna <paradon@wisdomlanna.com>
2016-10-03 10:25:21 +0700

    Updated reservation management

--- application/views/admin/reservation/edit_reservation.php
+++ application/views/admin/reservation/edit_reservation.php
@@ -158,7 +158,7 @@
                                     <h3 class="box-title"><?= $reservation_show[$key]['room_name'] ?></h3>
                                     <div class="box-tools pull-right">
                                         <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
-                                                class="fa fa-plus"></i>
+                                                class="fa fa-minus"></i>
                                         </button>
                                     </div>
                                 </div><!-- /.box-header -->
